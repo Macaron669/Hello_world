4ea9ae6f4939c94083cffacbe05304c4875e9672
Macaron669 <c.godzom@free.fr>
2021-02-27 00:14:05 +0100

    test pour vérifier si je peux ajouter mes exercices sur Github

---
+++ Exercice UpyLaB 2.1.py
@@ -0,0 +1,13 @@
+""" exercice débuté le 15 février 2021 demandé par apprendre à coder avec Python
+"""
+
+x=36
+produit=x*x
+print(produit)
+
+"""2.5.7. Exercice UpyLaB 2.3"""
+
+a=float(input())
+b=float(input())
+c=float(input())
+print((b*c)/a)
